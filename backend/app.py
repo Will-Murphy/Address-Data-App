303c9c71b333d3e962b72efcc318ad3a13c4e1ca
Will Murphy <williammurphy9723@gmail.com>
2020-05-05 17:19:22 -0400

    finish stream backend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request
 
-import address_service_wrapper as address_service
+from address_service_wrapper import AddressServiceWrapper
 #from flask_cors import CORS
 
 app = Flask(__name__)
+address_service = AddressServiceWrapper(config = "./address_microservice/config.cfg")
 #CORS(app)
 
 @app.route('/')
@@ -15,14 +16,17 @@ def hello():
 def geocode_one(address): 
     return address_service.geocode_one(address)
 
+
 @app.route('/validate/<string:address>')
 def validate_one(address):
     return address_service.validate_one(address)
+
 
 #TODO: removed parammeters for testing 
 @app.route('/validate-geocode/<string:address>')
 def validate_and_geocode_one(address):
     return address_service.validate_and_geocode_one(address)
+
 
 @app.route('/reverse-geocode/<string:coordinates>')
 def reverse_geocode_one(coordinates):
